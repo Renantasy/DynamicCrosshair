39fedeaf3b870cea5a2b3e76e0f55977fbd4e43b
Renantasy <95888743+Renantasy@users.noreply.github.com>
2021-12-30 19:12:31 -0300

    Add files via upload

---
+++ DynamicCursor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicCrosshair : MonoBehaviour
+{
+    public GameObject crosshair;
+    bool mouseOver = false;
+
+    void OnTriggerEnter(Collider jennyCollider)
+    {
+        if(jennyCollider.GetComponent<Collider>().tag == "Player" && mouseOver == true)
+        {
+            crosshair.SetActive(true);
+        }
+
+        else
+        {
+            crosshair.SetActive(false);
+        }
+    }
+
+    void OnTriggerExit(Collider jennyCollider)
+    {
+        crosshair.SetActive(false);
+    }
+
+    void OnMouseOver()
+    {
+        mouseOver = true;
+    }
+
+    void OnMouseExit()
+    {
+        mouseOver = false;
+    }
+}
